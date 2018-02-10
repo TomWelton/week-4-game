aa675cb8e98af41c3092d155710d38c0a68023fe
TomWelton <twelton1228@gmail.com>
2018-02-09 21:01:26 -0600

    2/9 new edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,16 @@
-//Crystal Game Javascript and jQuery file
-//
+//Crystal Game Javascript
 
 //Variables
-var targetNumber = 50;
+//selects number 19-120 for computer to use as target
+var targetNumber = Math.floor((Math.random() * 100) + 20);
+//user score starts at zero
 var counter = 0;
-var numberOptions = [10, 5, 3, 7];
-var increment = numberOptions[Math.round(Math.random())];
-
+//array created to hold user-clickable button values to achieve score
+var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//used to total and show multiple clicks of value for a "total score" for user
+var increment = numberOptions;
 //Number user is trying to guess
+
 $("#number-to-guess").text(targetNumber);
 
 
@@ -22,4 +25,5 @@ $(".crystalPic").on("click", function () {
     } else if (counter >= targetNumber) {
         alert("You Lose!");
     }
+
 });
