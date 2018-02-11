45b41633b4354b8aea103b2203f1a0158956b6e3
TomWelton <twelton1228@gmail.com>
2018-02-10 21:11:51 -0600

    Functional game updatesgit add -A

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,46 @@
 //Crystal Game Javascript
-
+///////////////////////////////////////////
+// DONT FORGET DOCUMENT READY FUNCTIONS
+///////////////////////////////////////////
 //Variables
-//selects number 19-120 for computer to use as target
-var targetNumber = Math.floor((Math.random() * 100) + 20);
-//user score starts at zero
-var counter = 0;
-//array created to hold user-clickable button values to achieve score
-var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-//used to total and show multiple clicks of value for a "total score" for user
-var increment = numberOptions;
-//Number user is trying to guess
-
-$("#number-to-guess").text(targetNumber);
-
-
-$(".crystalPic").on("click", function () {
-
-    counter += increment;
-    //User's total combined guesses
-    $("#userScore").text(counter);
-
-    if (counter === targetNumber) {
-        alert("You Win!");
-    } else if (counter >= targetNumber) {
-        alert("You Lose!");
+$(document).ready(function () {
+    //selects number 19-120 for computer to use as target
+    var targetNumber = Math.floor((Math.random() * 100) + 20);
+    //user score starts at zero
+    var counter = 0;
+    //array created to hold user-clickable button values to achieve score
+    var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    //used to total and show multiple clicks of value for a "total score" for user
+    var increment = numberOptions;
+	//array to hold the four selected numbers.
+	var selectedNumbers = [0, 0, 0, 0];
+    
+	//alert("Tommy");
+	//Console.log("Tommy");
+	
+	//Number user is trying to guess
+    for (var i = 0; i < 4; i++) {
+        selectedNumbers[i] = numberOptions[Math.floor(Math.random() * 12)];
+		//alert("selectedNumbers[" + i + "]: " + selectedNumbers[i]);
     }
 
+
+    $("#number-to-guess").text(targetNumber);
+
+
+    $(".crystalPic").on("click", function () {
+
+		//alert(this.id);
+        counter += selectedNumbers[this.id-1];
+        //User's total combined guesses
+        $("#userScore").text(counter);
+
+        if (counter == targetNumber) {
+            alert("You Win!");
+			window.location.reload();
+        } else if (counter >= targetNumber) {
+            alert("You Lose!");
+			window.location.reload();
+        }
+    });
 });
